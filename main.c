2742a7347988db6102d93b6167aea033b0c05e2f
Selim Efe <uysalselimefe@gmail.com>
2026-01-13 17:35:04 +0300

    Implement random prime number generation logic
    Added source code that prompts the user for a count (N), generates random integers using srand/rand, and validates primality by excluding 0/1 and checking divisors.

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+        int n,i,asal,sum,sayi,j;
+
+    printf("n i giriniz");
+    scanf("%d",&n);
+
+    sum=1;
+
+    srand(time(NULL));
+
+    for(;;){
+        asal=1;
+
+        sayi=rand();
+
+        for(j=2;j<sayi;j++){
+            if(sayi%j==0){
+                asal=0;
+                break;
+            }
+        }
+        if(asal==1){
+            printf("%d. sayi: %d\n",sum,sayi);
+            sum++;
+        }
+        if(sum==n+1){
+            break;
+        }
+    }
+
+}
