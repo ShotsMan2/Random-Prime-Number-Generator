33c1d9b8f09a50c05ce217f0c93c31c4f3c6eb9a
Selim Efe <uysalselimefe@gmail.com>
2026-01-17 22:14:50 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,36 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main()
 {
-        int n,i,asal,sum,sayi,j;
+    int n, i, asal, sum, sayi, j;
 
     printf("n i giriniz");
-    scanf("%d",&n);
+    scanf("%d", &n);
 
-    sum=1;
+    sum = 1;
 
     srand(time(NULL));
 
-    for(;;){
-        asal=1;
+    for (;;)
+    {
+        asal = 1;
 
-        sayi=rand();
+        sayi = rand();
 
-        for(j=2;j<sayi;j++){
-            if(sayi%j==0){
-                asal=0;
+        for (j = 2; j < sayi; j++)
+        {
+            if (sayi % j == 0)
+            {
+                asal = 0;
                 break;
             }
         }
-        if(asal==1){
-            printf("%d. sayi: %d\n",sum,sayi);
+        if (asal == 1)
+        {
+            printf("%d. sayi: %d\n", sum, sayi);
             sum++;
         }
-        if(sum==n+1){
+        if (sum == n + 1)
+        {
             break;
         }
     }
-
 }
